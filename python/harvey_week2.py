483ef1bfa2bc7136441010b9df8c468f060acdba
haru3613 <qaw8975889@gmail.com>
2021-04-21 22:22:13 +0800

    problem1 not finish

--- python/harvey_week2.py
+++ python/harvey_week2.py
@@ -18,3 +18,21 @@ def longestCommonPrefix(self, strs: List[str]) -> str:
         else:
             return prefix[:-1]
 
+
+def romanToInt(self, s: str) -> int:
+    symbols = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    print(list(symbols.keys()).index("I"))
+    result = 0
+    
+    for char in s:
+        result += symbols[char]
+        
+    return result
