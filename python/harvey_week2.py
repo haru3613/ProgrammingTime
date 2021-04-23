2c2d539d2e40cb72b6ff13d038b741c7f1fd77c5
Harvey_Chan <harvey_chan@talfin.ai>
2021-04-23 13:13:45 +0800

    completed

--- python/harvey_week2.py
+++ python/harvey_week2.py
@@ -21,18 +21,22 @@ def longestCommonPrefix(self, strs: List[str]) -> str:
 
 def romanToInt(self, s: str) -> int:
     symbols = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    print(list(symbols.keys()).index("I"))
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
     result = 0
-    
-    for char in s:
-        result += symbols[char]
+    for index, char in enumerate(s):
+        try:
+            if symbols[char] < symbols[s[index+1]]:
+                result -= symbols[char]
+            else:
+                result += symbols[char]
+        except:
+            result += symbols[char]
         
     return result
